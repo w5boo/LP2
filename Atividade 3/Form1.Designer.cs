913d4c4485d8b0686b3773ba07f73e2b86133fc1
w5boo <69856789+w5boo@users.noreply.github.com>
2021-05-17 20:53:35 -0300

    Add files via upload

---
+++ Atividade 3/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace LadosDoTriangulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnVerificar = new System.Windows.Forms.Button();
+            this.lblA = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.LblB = new System.Windows.Forms.Label();
+            this.LblC = new System.Windows.Forms.Label();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.BtnLimpar = new System.Windows.Forms.Button();
+            this.BtnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BtnVerificar
+            // 
+            this.BtnVerificar.Location = new System.Drawing.Point(986, 89);
+            this.BtnVerificar.Name = "BtnVerificar";
+            this.BtnVerificar.Size = new System.Drawing.Size(223, 125);
+            this.BtnVerificar.TabIndex = 0;
+            this.BtnVerificar.Text = "Verificar";
+            this.BtnVerificar.UseVisualStyleBackColor = true;
+            this.BtnVerificar.Click += new System.EventHandler(this.BtnVerificar_Click);
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(135, 89);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(471, 50);
+            this.lblA.TabIndex = 1;
+            this.lblA.Text = "Insira o lado A do triângulo";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(135, 166);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(150, 57);
+            this.txtA.TabIndex = 2;
+            // 
+            // LblB
+            // 
+            this.LblB.AutoSize = true;
+            this.LblB.Location = new System.Drawing.Point(135, 256);
+            this.LblB.Name = "LblB";
+            this.LblB.Size = new System.Drawing.Size(468, 50);
+            this.LblB.TabIndex = 3;
+            this.LblB.Text = "Insira o lado B do triângulo";
+            // 
+            // LblC
+            // 
+            this.LblC.AutoSize = true;
+            this.LblC.Location = new System.Drawing.Point(135, 445);
+            this.LblC.Name = "LblC";
+            this.LblC.Size = new System.Drawing.Size(470, 50);
+            this.LblC.TabIndex = 4;
+            this.LblC.Text = "Insira o lado C do triângulo";
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(135, 345);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(150, 57);
+            this.txtB.TabIndex = 5;
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(135, 529);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(150, 57);
+            this.txtC.TabIndex = 6;
+            // 
+            // BtnLimpar
+            // 
+            this.BtnLimpar.Location = new System.Drawing.Point(986, 291);
+            this.BtnLimpar.Name = "BtnLimpar";
+            this.BtnLimpar.Size = new System.Drawing.Size(223, 125);
+            this.BtnLimpar.TabIndex = 7;
+            this.BtnLimpar.Text = "Limpar";
+            this.BtnLimpar.UseVisualStyleBackColor = true;
+            this.BtnLimpar.Click += new System.EventHandler(this.BtnLimpar_Click);
+            // 
+            // BtnSair
+            // 
+            this.BtnSair.Location = new System.Drawing.Point(986, 485);
+            this.BtnSair.Name = "BtnSair";
+            this.BtnSair.Size = new System.Drawing.Size(223, 125);
+            this.BtnSair.TabIndex = 8;
+            this.BtnSair.Text = "Sair";
+            this.BtnSair.UseVisualStyleBackColor = true;
+            this.BtnSair.Click += new System.EventHandler(this.BtnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 50F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1397, 678);
+            this.Controls.Add(this.BtnSair);
+            this.Controls.Add(this.BtnLimpar);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.LblC);
+            this.Controls.Add(this.LblB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.BtnVerificar);
+            this.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnVerificar;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.Label LblB;
+        private System.Windows.Forms.Label LblC;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Button BtnLimpar;
+        private System.Windows.Forms.Button BtnSair;
+    }
+}
+
---
+++ Atividade 3/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LadosDoTriangulo
+{
+    public partial class Form1 : Form
+    {
+
+        double ladoA, ladoB, ladoC;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnVerificar_Click(object sender, EventArgs e)
+        {
+            if (double.TryParse(txtA.Text, out ladoA) &&
+                double.TryParse(txtB.Text, out ladoB) &&
+                double.TryParse(txtC.Text, out ladoC))
+
+
+            {
+                if (Math.Sqrt((ladoB - ladoC) * (ladoB - ladoC)) < ladoA && ladoA < ladoB + ladoC &&
+                   (Math.Sqrt((ladoA - ladoC) * (ladoA - ladoC)) < ladoB && ladoB < ladoA + ladoC &&
+                   (Math.Sqrt((ladoA - ladoB) * (ladoA - ladoB)) < ladoC && ladoC < ladoA + ladoB)))
+                {
+                    if ((ladoA == ladoB) && (ladoC == ladoB))
+                    {
+                        MessageBox.Show("O triângulo formado é Equilátero");
+                    }
+
+                    else
+
+                    {
+
+                        if ((ladoA != ladoB) && (ladoB != ladoC) && (ladoC != ladoA))
+                        {
+                            MessageBox.Show("O triângulo formado é Escaleno");
+                        }
+                        else
+                        {
+                            MessageBox.Show("O triângulo formado é Isósceles");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void BtnLimpar_Click(object sender, EventArgs e)
+        {
+            txtA.Clear();
+            txtB.Clear();
+            txtC.Clear();
+        }
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
